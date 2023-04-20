9dd28d7b4b82dd7902b7f9c83fd3f605e2a75b61
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-04-20 11:52:59 +0200

    Preparing the tasks controller & routes

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks.routes");
 
 const port = 3000;
+
+// middleware
+
+app.use(express.json());
 
 // routes
 app.get("/hello", (req, res) => {
   res.send("Task Mangaer App");
 });
 
+app.use("/api/v1/tasks", tasks);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
