e5fe5266263f9d0d8a0d1e7c9b3d1256fca78c8f
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-04-20 05:49:01 +0200

    Project initialization

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Mangaer App");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
